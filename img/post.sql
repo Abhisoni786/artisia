7335c500f4cbf00aecb00e30b513f743b75fccf4
Abhishek Soni <abhisoni883@gmail.com>
2019-06-15 21:49:06 +0530

    Add files via upload

---
+++ img/post.sql
@@ -0,0 +1,14 @@
+INSERT INTO `posts`(title,imge_url) values("Horses","http://localhost/acme/img/Sculpture/horses.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("couple","http://localhost/acme/img/Sculpture/couple.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("stars","http://localhost/acme/img/Sculpture/stars.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("mother","http://localhost/acme/img/Sculpture/mother.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("thinker","http://localhost/acme/img/Sculpture/thinker.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("statue","http://localhost/acme/img/Sculpture/statue.jpg");
+
+INSERT INTO `posts`(title,imge_url) values("horse","http://localhost/acme/img/Sculpture/horse.jpg");
+INSERT INTO `posts`(title,imge_url) values("girls","http://localhost/acme/img/Sculpture/girls.jpg");
